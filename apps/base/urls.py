126e70180fad04d349ce0d2db28259b6f1677dfe
Viktor <viktorkrilldj@gmail.com>
2023-02-22 19:45:52 -0500

    sds

--- apps/base/urls.py
+++ apps/base/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from apps.base.views import * 
 from django.conf import settings
 from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('', home, name="home"),
