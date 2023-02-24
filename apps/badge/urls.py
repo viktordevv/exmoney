e6cf182ceffa14350603f56596e9038d87451473
Viktor <viktorkrilldj@gmail.com>
2023-02-24 18:20:15 -0500

    bye

--- apps/badge/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from django.conf import settings
-from django.conf.urls.static import static
-
-
-urlpatterns = [
-    path('badge/', views.users, name="badge"),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
