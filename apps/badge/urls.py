a1e9b938e1e6065b5317a5b96e69bdc740953fe1
Viktor <viktorkrilldj@gmail.com>
2023-02-26 19:21:32 -0500

    delete

--- apps/badge/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-
-urlpatterns = [
-    path('list/', views.list, name="list"),
-    path('expand/', views.expand, name="expand"),
-    path('list_bank/', views.consulta, name="list_bank"),
-]
--- apps/convert/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, redirect
-
-def convert(request):
-    context = {}
-
-    return render(request, 'convert/convert.html', context)
-
-
